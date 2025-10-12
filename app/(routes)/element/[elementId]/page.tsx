967385487f0b7d85c0b7d6b581d89ef6ff0562ff
Hferpi <hferpi@gmail.com>
2025-10-12 16:45:55 +0200

    ultimisimos cambios segun chatGPT

--- app/(routes)/element/[elementId]/page.tsx
+++ app/(routes)/element/[elementId]/page.tsx
@@ -1,26 +1,37 @@
-import { getServerSession } from 'next-auth';
+import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
-import { db } from '@/lib/db';
-import { FormEditElement } from '@/components/Shared/FormEditElement';
+import { db } from "@/lib/db";
+import { FormEditElement } from "@/components/Shared/FormEditElement";
 
+type PageProps = {
+  params: {
+    elementId: string;
+  };
+};
 
-export default async function ElementPage({params}: {params: {elementId: string}}) {
-const sesion = await getServerSession();
-if( !sesion || !sesion.user?.email ) {
-    return redirect('/');
-}
-const element = await db.element.findUnique({
-where:{
-    id: params.elementId
-}
-})
-if(!element) {
-    redirect('/');
-}
-    return <div>
-        <h1>Element page</h1>
-        <div>
-<FormEditElement dataElement={element} />
-        </div>
-    </div>;
+export default async function ElementPage({ params }: PageProps) {
+  const session = await getServerSession();
+
+  if (!session || !session.user?.email) {
+    redirect("/");
+  }
+
+  const element = await db.element.findUnique({
+    where: {
+      id: params.elementId,
+    },
+  });
+
+  if (!element) {
+    redirect("/");
+  }
+
+  return (
+    <div>
+      <h1>Element page</h1>
+      <div>
+        <FormEditElement dataElement={element} />
+      </div>
+    </div>
+  );
 }
