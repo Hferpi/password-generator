13e6e5b6bed5eab063a915a23ebe3e58b6b61c14
Hferpi <hferpi@gmail.com>
2025-10-15 20:57:20 +0200

    recibir el onClick

--- components/Shared/SingleItem/SingleItem.tsx
+++ components/Shared/SingleItem/SingleItem.tsx
@@ -4,13 +4,13 @@ import { SingleItemProps } from "./SingleItem.types";
 
 
 export function SingleItem(props: SingleItemProps) {
-    const {label, href, icon: Icon, onclick} = props;
+    const { label, href, icon: Icon, onClick } = props;
     return (
-       <Link href={href} onClick={onclick} className="flex items-center gap-2 p-2 hover:bg-blue-100/20 duartaion-300 transition-all rounded-md">
-       <div className="bg-blue-100/20 p-2 rounded-md">
-<Icon size={20} />
-       </div>
-       {label}
-       </Link>
+        <Link href={href} onClick={onClick} className="flex items-center gap-2 p-2 hover:bg-blue-100/20 duartaion-300 transition-all rounded-md">
+            <div className="bg-blue-100/20 p-2 rounded-md">
+                <Icon size={20} />
+            </div>
+            {label}
+        </Link>
     )
 }
--- components/Shared/SingleItem/SingleItem.types.ts
+++ components/Shared/SingleItem/SingleItem.types.ts
@@ -4,5 +4,5 @@ export type SingleItemProps = {
     label: string;
     href: string;
     icon: LucideIcon;
-    onclick?: () => void;
+    onClick?: () => void;
 }
